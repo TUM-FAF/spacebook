a0fd8876aa6196389b650d94ea9e81ce5700767f
Dragoș Străinu <str.dr4605@gmail.com>
2019-10-19 22:50:06 +0300

    Add Header

---
+++ src/components/Header/Header.styled.ts
@@ -0,0 +1,7 @@
+import styled, { DefaultTheme, StyledComponent } from 'styled-components';
+
+export const Header: StyledComponent<'header', DefaultTheme> = styled.header`
+  padding-top: 20px;
+  padding-left: 30px;
+  height: 60px;
+`;
