5e4a09f542645f2862d676076a8e909688e38976
lcernei <cernei.liviu@gmail.com>
2019-10-20 13:37:12 +0300

    Refactor MainPage to fetch DayCards dynamically

--- src/components/DayCard/DayCard.tsx
+++ src/components/DayCard/DayCard.tsx
@@ -1,25 +1,25 @@
+import { DateTime } from 'luxon';
 import React from 'react';
-import { Logo } from '../../assets/icons/Logo';
-import BannerImage from '../../assets/resources/banner.png';
-
+import { IDayPicture } from '../../store';
 import * as s from './DayCard.styled';
 
-export const DayCard: React.FC = (): React.ReactElement => {
+interface IProps {
+  dayPicture: IDayPicture;
+}
+
+export const DayCard: React.FC<IProps> = (props: IProps): React.ReactElement => {
+  function readableDate(date: string): string {
+    return DateTime.fromISO(date).toFormat('dd LLL yyyy');
+  }
 
   return (
     <s.Container>
-      <s.Date>2019 October 20</s.Date>
-      <s.DayImageContainer />
+      <s.Date>{readableDate(props.dayPicture.date)}</s.Date>
+      <s.DayImageContainer url={props.dayPicture.url} />
       <s.InfoContainer>
-        <s.Title>
-          All Female Spacewalk Repairs Space Station
-        </s.Title>
-        <s.CopyRight>
-          Image Credit: NASA TV, Expedition 61
-        </s.CopyRight>
-        <s.Explanation>
-          Explanation: The failed unit was beyond the reach of the robotic Canadarm2.
-        </s.Explanation>
+        <s.Title>{props.dayPicture.title}</s.Title>
+        <s.CopyRight>{props.dayPicture.copyright}</s.CopyRight>
+        <s.Explanation>{props.dayPicture.explanation}</s.Explanation>
       </s.InfoContainer>
     </s.Container>
   );
