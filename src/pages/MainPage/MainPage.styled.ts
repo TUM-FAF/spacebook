8d4cd3a2c087e331755774cc691cabe89368536d
adrianncovaci <cpaadrian2@gmail.com>
2019-10-20 13:03:30 +0300

    Add Responsive

--- src/pages/MainPage/MainPage.styled.ts
+++ src/pages/MainPage/MainPage.styled.ts
@@ -1,9 +1,10 @@
 import styled, { DefaultTheme, StyledComponent } from 'styled-components';
-import BannerImage from '../../assets/resources/banner.png';
-
 
 export const Container: StyledComponent<'div', DefaultTheme> = styled.div`
   font-family: 'DM Serif Display';
-  max-width: 375px;
   min-width: 375px;
+
+  @media only screend and (min-width:768px) {
+    width: 800px;
+  } 
 `;
