a0d16a849034fab0eb42e4c1fc46a63785a5954f
Dragoș Străinu <str.dr4605@gmail.com>
2019-10-27 01:35:30 +0300

    Fix bug when picture not published for current day

--- src/components/DayCard/DayCard.tsx
+++ src/components/DayCard/DayCard.tsx
@@ -2,31 +2,35 @@ import { DateTime } from 'luxon';
 import React from 'react';
 // @ts-ignore
 import ShowMoreText from 'react-show-more-text';
-import { IDayPicture } from '../../store';
+import { IDayPicture, INoPicture, isDayPicture } from '../../store';
 import * as s from './DayCard.styled';
 
 interface IProps {
-  dayPicture: IDayPicture;
+  dayPicture: IDayPicture | INoPicture;
 }
 
-export const DayCard: React.FC<IProps> = (props: IProps): React.ReactElement => {
+export const DayCard: React.FC<IProps> = (props: IProps): React.ReactElement | null => {
   function readableDate(date: string): string {
     return DateTime.fromISO(date).toFormat('dd LLLL yyyy');
   }
 
   return (
     <s.Container>
-      <s.Date>{readableDate(props.dayPicture.date)}</s.Date>
-      <s.DayImageContainer url={props.dayPicture.url} />
-      <s.InfoContainer>
-        <s.Title>{props.dayPicture.title}</s.Title>
-        {!!props.dayPicture.copyright && <s.CopyRight>Image Credit: {props.dayPicture.copyright}</s.CopyRight>}
-        <s.Explanation>
-          <ShowMoreText lines={1} more={'Show more'} less={'Show less'}>
-            {props.dayPicture.explanation}
-          </ShowMoreText>
-        </s.Explanation>
-      </s.InfoContainer>
+      { isDayPicture(props.dayPicture) ? (
+        <>
+          <s.Date>{readableDate(props.dayPicture.date)}</s.Date>
+          <s.DayImageContainer url={props.dayPicture.url} />
+          <s.InfoContainer>
+            <s.Title>{props.dayPicture.title}</s.Title>
+            {!!props.dayPicture.copyright && <s.CopyRight>Image Credit: {props.dayPicture.copyright}</s.CopyRight>}
+            <s.Explanation>
+              <ShowMoreText lines={1} more={'Show more'} less={'Show less'}>
+                {props.dayPicture.explanation}
+              </ShowMoreText>
+            </s.Explanation>
+          </s.InfoContainer>
+        </>
+      ) : null }
     </s.Container>
   );
 };
